0097e1ceee0b2b074efaea7978bcce05be606296
Symforian <adrienkaminskig@gmail.com>
2020-01-01 18:03:54 +0100

    CLI with data modyfication

--- graph.py
+++ graph.py
@@ -6,14 +6,6 @@ from Family import Family as f
 
 
 class graph_representation:
-    node_attr = {
-    }
-    child_attr = {
-    }
-    marr_attr = {
-        "style": "tapered",
-        "arrowhead": "none"
-    }
 
     def __init__(self, data=None):
         self.data = data
@@ -22,27 +14,99 @@ class graph_representation:
     def upload_data(self, data):
         self.data = data
 
+    def get_originless_partners(self, idn):
+        partners = self.get_partners(idn)
+        result = set()
+        if partners is not None:
+            for partner_id in partners:
+                partner = self.data[partner_id]
+                if partner.origin is None:
+                    result.add(partner_id)
+        return result
+
+    def get_partners(self, idn):
+        fam_c_ids = self.data[idn].family_connections
+        if fam_c_ids is None:
+            return None
+        else:
+            ids = set()
+            for fam_c_id in fam_c_ids:
+                family = self.data[fam_c_id]
+                if family.head == idn:
+                    ids.add(family.partner)
+                else:
+                    ids.add(family.head)
+        return ids
+
     def find_top(self):
         for entry in self.data.values():
             if isinstance(entry, p) and entry.origin is None:
-                self.current_level.add(entry.idn)
+                part_with_origin = False
+                partners = self.get_partners(entry.idn)
+                if partners is not None:
+                    for partner_id in list(partners):
+                        partner = self.data[partner_id]
+                        if partner.origin is not None:
+                            part_with_origin = True
+                            break
+                if not part_with_origin:
+                    self.current_level.add(entry.idn)
+
+    def draw_nodes(self, el):
+        # if they have a family
+        if el.family_connections is not None:
+            for fam_idn in el.family_connections:
+                self.subtree.node(fam_idn, shape="point")
+                self.subtree.node(el.idn, el.clean_display())
+        else:
+            self.subtree.node(el.idn, el.clean_display())
+
+    def get_children(self, el, children):
+        # if they have a family
+        if el.family_connections is not None:
+            for fam_idn in el.family_connections:
+                family = self.data[fam_idn]
+                # if there are children in this family
+                if family.family_connections is not None:
+                    for c in family.family_connections:
+                        children.add(c)
+        return children
+
+    def get_side_nodes(self, el, cur_origls_p, diff=None):
+        originless = self.get_originless_partners(el.idn)
+        if diff is not None:
+            originless = originless.difference(diff)
+        cur_origls_p = cur_origls_p.union(originless)
+        return cur_origls_p
+
+    def create_node(self, orig, next_lv, guardian=None):
+        if guardian is None:
+            data = self.current_level
+        else:
+            data = guardian
+        while not len(data) == 0:
+            el = self.data[data.pop()]
+            orig = self.get_side_nodes(el, orig, guardian)
+            next_lv = self.get_children(el, next_lv)
+            self.draw_nodes(el)
+        if guardian is None:
+            self.current_level = set()
+            return (orig, next_lv)
+        return next_lv
 
     def create_nodes(self):
+        # TODO make sure the children are under their parents
+        # maybe change set to list or something
+        print("Current lv:" + str(self.current_level))
         next_level = set()
-        with self.tree.subgraph() as level:
-            level.attr(rank='same')
-            while not len(self.current_level) == 0:
-                el = self.data[self.current_level.pop()]
-                if el.family_connections is not None:
-                    for fam_idn in el.family_connections:
-                        family = self.data[fam_idn]
-                        level.node(fam_idn, shape="point")
-                        level.node(el.idn, el.clean_display())
-                        for c in family.family_connections:
-                            print(next_level)
-                            next_level.add(c)
-                else:
-                    level.node(el.idn, el.clean_display())
+        originless_part = set()
+        self.subtree = Tree()
+        self.subtree.attr(rank='same')
+        o, nl = self.create_node(originless_part, next_level)
+        originless_part, next_level = o, nl
+        ptnrs_g = originless_part
+        next_level = self.create_node(originless_part, next_level, ptnrs_g)
+        self.tree.subgraph(self.subtree)
         if len(next_level) != 0:
             self.current_level = next_level
             self.create_nodes()
@@ -52,8 +116,9 @@ class graph_representation:
             if isinstance(entry, f):
                 self.tree.edge(entry.head, entry.idn, arrowhead="none")
                 self.tree.edge(entry.idn, entry.partner, arrowhead="none")
-                for c in entry.family_connections:
-                    self.tree.edge(entry.idn, c)
+                if entry.family_connections is not None:
+                    for c in entry.family_connections:
+                        self.tree.edge(entry.idn, c)
 
     def show(self):
         self.tree = Tree()
--- program.py
+++ program.py
@@ -1,5 +1,8 @@
+from argparse import ArgumentParser as ap
 from graph import graph_representation as g_r
 from gedcom_parser import GEDCOM_parser as ged_parse
+from Person import Person as per
+from Family import Family as fam
 
 
 class program:
@@ -7,14 +10,114 @@ class program:
     def __init__(self):
         self.env = None
         self.drawer = g_r()
-        pass
+        self.running = True
 
-    def import_gedcom_show_only(self, filename):
-        parser = ged_parse(filename)
+    def import_gedcom(self):
+        parser = ged_parse(input("Enter filename\n"))
         self.env = parser.parse()
+
+    def show(self):
         self.drawer.upload_data(self.env.entries())
         self.drawer.show()
 
+    def list_env(self):
+        part = input("What do you want to list?\n")
+        if self.env is None:
+            print("Empty")
+        if part is None:
+            return self.env.entries()
+        items = list(self.env.entries().items())
+        if part in ('p', 'person', 'persons'):
+            print(list(filter(lambda t: isinstance(t[1], per), items)))
+        elif part in ('f', 'family', 'families'):
+            print(list(filter(lambda t: isinstance(t[1], fam), items)))
 
-p = program()
-p.import_gedcom_show_only('km.GED')
+    def add_entry(self):
+        i = input("Enter idn:\n")
+        n = [input("Enter name:\n")]
+        s = [input("Enter surname:\n")]
+        bd = input("Enter birth day:\n")
+        bm = input("Enter birth month:\n")
+        by = input("Enter birth year:\n")
+        b = [bd, bm, by]
+        d = input("Enter death day:\n")
+        new_person = per(idn=i, name=n, sname=s, birt=b, deat=d)
+        self.env.entries()[i] = new_person
+
+    def check_entry(self):
+        idn = input("Enter idn:\n")
+        print(self.env.entries()[idn])
+
+    def remove_entry(self):
+        idn = input("Enter idn:\n")
+        self.env.entries().pop(idn)
+
+    def connect(self):
+        idn = input("Enter idn:\n")
+        h = input("Enter head id:\n")
+        p = input("Enter partner id:\n")
+        r = input("Enter relation name:\n")
+        self.env.entries()[idn] = fam(idn, h, p, r)
+        # TODO if one of h p is None, create Unknown <- but only one
+        self.env.entries()[h].add(idn)
+        self.env.entries()[p].add(idn)
+
+    def update_entry(self):
+        idn = input("Enter idn:\n")
+        data = input("Enter data:\n")
+        value = input("Enter value:\n")
+        if data in ('birth', 'death'):
+            value = value.split()
+        elif data == 'origin':
+            self.env.entries()[value].add(idn)
+        self.env.entries()[idn].updateData(data, value)
+
+    def quit(self):
+        self.running = False
+
+    def error():
+        print("Unknown command")
+    commands = {
+                'i': import_gedcom,
+                's': show,
+                'l': list_env,
+                'a': add_entry,
+                'c': check_entry,
+                'r': remove_entry,
+                'u': update_entry,
+                'm': connect,
+                'q': quit,
+                }
+
+
+def main_cli():
+    p = program()
+    DEFINED_GEDFILE = 'km.GED'
+    current_command = None
+    while(p.running):
+        if current_command is None:
+            current_command = p.commands.get(input("Enter command:"), p.error)
+            if current_command.__name__ == 'error':
+                current_command = None
+            if current_command is not None:
+                getattr(p, current_command.__name__)()
+                current_command = None
+
+
+def main_gui():
+    # TODO implementu GUI
+    print("Not yet implemented")
+    return 0
+
+
+if __name__ == "__main__":
+    parser = ap(description='Choose program mode')
+    parser.add_argument('--command_line', '-c', action='store_const',
+                        const=True, default=False,
+                        help='enter CLI mode (default: GUI mode)')
+    args = parser.parse_args()
+    if vars(args)['command_line']:
+        main_cli()
+    else:
+        main_gui()
+
